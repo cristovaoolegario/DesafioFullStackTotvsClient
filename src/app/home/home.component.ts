9a765d32f96453e9a3b86e84ca8e7967862b9023
Cristóvão Olegário <cristovaoolegario@gmail.com>
2018-12-10 02:33:12 -0200

    Adição do AuctionService na HomePage.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,25 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 
-import { User } from '../_models';
-import { UserService } from '../_services';
+import { User, Auction } from '../_models';
+import { UserService, AuctionService } from '../_services';
 
 @Component({templateUrl: 'home.component.html'})
 export class HomeComponent implements OnInit {
     users: User[] = [];
+    auctions: Auction[] = []
 
-    constructor(private userService: UserService) {}
+    constructor(
+        private userService: UserService,
+        private auctionService: AuctionService
+        ) {}
 
     ngOnInit() {
         this.userService.getAll().pipe(first()).subscribe(users => { 
             this.users = users; 
         });
+        this.auctionService.getAll().pipe(first()).subscribe(auctions => {
+            this.auctions = auctions;
+        })
     }
 }
